7ee3b1f30c6dfa51473d5c690144382e7658bd21
allgoliot <allan.goliot@gmail.com>
2019-12-02 13:17:39 +0100

    Modification du ficher d'installation de docker apt

--- docker/install_docker_apt.sh
+++ docker/install_docker_apt.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
-sudo apt-get install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -18,7 +18,7 @@ sudo add-apt-repository \
    stable"
 
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
