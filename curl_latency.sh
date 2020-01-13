a69f0011e5ab796c37646f5b9042bdd6d2cda646
root <root@test.us-central1-a.c.golden-furnace-233615.internal>
2020-01-13 13:40:56 +0000

    Modification de curl_latency et push

--- curl_latency.sh
+++ curl_latency.sh
@@ -1,12 +1,15 @@
 #/bin/bash
 if [ $# == 0 ];then
         echo "Usage : $0 ip_a_tester"
-else
+else 
+	command -V  bc &>/dev/null
+        if [ $? -ne 0 ]; then
+          command apt-get install bc
+	  echo "j'ai installé"
+        fi
         ip=$1
-
-
-for ((i=1;i<=10;i++))
-do
-        echo total_time: $(echo "`curl -s -o /dev/null -w '1000*%{time_total}\n' -s http://$ip` " | bc) "ms"
-done
+	for ((i=1;i<=10;i++))
+		do
+			echo total_time: $(echo "`curl -s -o /dev/null -w '1000*%{time_total}\n' -s http://$ip` " | bc) "ms"
+		done
 fi
--- push.sh
+++ push.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 git add .
-git commit -m "auto commit"
-git push origin master
+git status
+read -p "Etes-vous sûrs de vouloir push? (y/n): " reponse
+if [ $reponse ==  "y" ];then
+	read -p "Commentaire de commit : " commentaire
+	git commit -m "`echo $commentaire`"
+#	git push origin master
+fi
+
+echo $commentaire
